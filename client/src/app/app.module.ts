66ef26b031da9a68c746f9f5573f474c41c9cf63
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-04-05 10:45:17 +0700

    Create header component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SmartBarComponent } from './components/smart-bar/smart-bar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SmartBarComponent
+    SmartBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
